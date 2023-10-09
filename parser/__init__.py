6145591ed6c23665f605176e2a2308e30c0565f1
1FaKe110 <gabko2016@gmail.com>
2023-10-09 17:58:52 +0300

    исправил поиск вхождений теперь найденное(ые) словосочетание удаляется из предложения

--- parser/__init__.py
+++ parser/__init__.py
@@ -12,84 +12,100 @@ DetectorFactory.seed = 0
 from parser.nlps import Nlps
 
 
+class Text:
+
+    def __init__(self, text: str):
+        self.text = text
+        self.sentences = [Sentence(x) for x in re.split(r'(?<=[.!?])\s+', text)]
+
+
 class Sentence:
 
     def __init__(self, text):
         self.lang = detect(text)
 
         self.text: str = text
-        self.clear_sentence = text.translate(
-            str.maketrans('', '', string.punctuation)).lower()  # Удаление знаков препинания
         self.__nlp = Nlps().__getattribute__(self.lang)
+        self.lemmatized = None
 
-        self.lemmatized = self.lemmatize(text)
+    def clear_full_sentence(self):
+        return self.text.translate(str.maketrans('', '', string.punctuation)).lower()
 
     def lemmatize(self, text) -> str:
-        return " ".join([token.lemma_ for token in self.__nlp(text)])
+        return " ".join([token.lemma_ for token in self.__nlp(text)]).lower()
 
     def match_exact(self, phrase: str) -> DefaultMunch:
-        count = 0
-        words = self.clear_sentence.split()  # Разбиваем предложение на отдельные слова
-        phrase_length = len(phrase.split())  # Получаем длину словосочетания
+        phrase = phrase.lower()
+        logger.debug(f"Phrase: {phrase}")
 
-        # Проходим по всем словам в предложении
-        for i in range(len(words) - phrase_length + 1):
-            part = ' '.join(words[i:i + phrase_length])
-            if part == phrase.lower():
-                count += 1
+        sentence = self.clear_full_sentence()
+        matches = re.findall(phrase, sentence)
+
+        modified_sentence = re.sub(r'\b' + re.escape(phrase) + r'\b', '', sentence)
+        self.lemmatized = self.lemmatize(modified_sentence)
+        logger.debug(f'Исходный: {sentence}')
+        logger.debug(f'Лемма: {self.lemmatized}')
 
         # Вернем словарь, где будет ключом будет словосочетание, а значением кол-во
-        return as_class(dict(phrase=phrase, count=count))
+        return as_class(dict(phrase=phrase, count=len(matches)))
 
     def match_exactlemmed(self, phrase: str) -> DefaultMunch:
-        count = 0
-        words = self.lemmatized.split()
-        lemma_phrase = self.lemmatize(phrase).lower()
-        phrase_length = len(lemma_phrase.split())
+        lemma_phrase = self.lemmatize(phrase)
+        sentence = self.lemmatized
 
-        for i in range(len(words) - phrase_length + 1):
-            if ' '.join(words[i:i + phrase_length]) == lemma_phrase:
-                count += 1
+        logger.debug(f"Phrase: {lemma_phrase}")
 
-        return as_class(
-            dict(phrase=phrase,
-                 count=count)
-        )
+        matches = re.findall(r'\b' + re.escape(lemma_phrase) + r'\b', sentence)
+        self.lemmatized = re.sub(r'\b' + re.escape(phrase.lower()) + r'\b', '', sentence)
+        logger.debug(f'Лемма: {self.lemmatized}')
+        # Вернем словарь, где будет ключом будет словосочетание, а значением кол-во
+        return as_class(dict(phrase=phrase, count=len(matches)))
 
     def match_participant(self, phrase: str) -> DefaultMunch:
-        lemma_phrase = self.lemmatize(phrase).lower()
+        lemma_phrase = self.lemmatize(phrase)
+        sentence = self.lemmatized
+        logger.debug(f"Phrase: {lemma_phrase}")
         str_pattern = r'\b' + r'\b.*?\b'.join(map(re.escape, lemma_phrase.split())) + r'\b'
-        pattern = re.compile(str_pattern)
-        logger.debug(f'Result: {re.findall(pattern, self.lemmatized)}')
-        return as_class(
-            dict(phrase=phrase,
-                 count=len(re.findall(pattern, self.lemmatized)))
-        )
+
+        return self.re_search_lemmed(lemma_phrase, phrase, sentence, str_pattern)
 
     def match_imprecise(self, phrase: str) -> DefaultMunch:
-        lemma_phrase = self.lemmatize(phrase).lower().replace(' ', '|')
+        lemma_phrase = self.lemmatize(phrase).replace(' ', '|')
+        sentence = self.lemmatized
+
+        logger.debug(f"Phrase: {lemma_phrase}")
         str_pattern = r'\w*?(' + lemma_phrase + ')'
+
+        return self.re_search_lemmed(lemma_phrase, phrase, sentence, str_pattern)
+
+    def re_search_lemmed(self, lemma_phrase, phrase, sentence, str_pattern):
         pattern = re.compile(str_pattern)
-        return as_class(
-            dict(phrase=phrase,
-                 count=len(re.findall(pattern, self.lemmatized)))
-        )
+        matches = re.findall(pattern, sentence)
+        self.lemmatized = re.sub(r'\b' + re.escape(lemma_phrase) + r'\b', '', sentence)
+        logger.debug(f'Лемма: {self.lemmatized}')
+        return as_class(dict(phrase=phrase, count=len(matches)))
+
 
 def main():
-    # sentence = Sentence("This is the sentence with given sentence word phrases,"
-    #                     " so it sentence can be found word phrase")
-    sentence = Sentence("This is the sentence with given sentence phrases")
-    phrase = "sentence word phrase"
+    text = Text("""Bitcoin is the world’s first successful decentralized cryptocurrency and payment system, launched in 2009 by a mysterious creator known only as Satoshi Nakamoto. The word “cryptocurrency” refers to a group of digital assets where transactions are secured and verified using cryptography – a scientific practice of encoding and decoding data. Those transactions are often stored on computers distributed all over the world via a distributed ledger technology called blockchain (see below.)
+Bitcoin can be divided into smaller units known as “satoshis” (up to 8 decimal places) and used for payments, but it’s also considered a store of value like gold. This is because the price of a single bitcoin has increased considerably since its inception – from less than a cent to tens of thousands of dollars. When discussed as a market asset, bitcoin is represented by the ticker symbol BTC.
+The term “decentralized” is used often when discussing cryptocurrency, and simply means something that is widely distributed and has no single, centralized location or controlling authority. In the case of bitcoin, and indeed many other cryptocurrencies, the technology and infrastructure that govern the creation, supply, and security of it do not rely on centralized entities, like banks and governments, to manage it.
+Ethereum network
+colorful pins on board
+Instead, Bitcoin is designed in such a way that users can exchange value with one another directly through a peer-to-peer network; a type of network where all users have equal power and are connected directly to each other without a central server or intermediary company acting in the middle. This allows data to be shared and stored, or bitcoin payments to be sent and received seamlessly between parties.
+The Bitcoin network (capital “B”, when referring to the network and technology, lower-case “b” when referring to the actual currency, bitcoin) is completely public, meaning anyone in the world with an internet connection and a device that can connect to it can participate without restriction. It’s also open-source, meaning anyone can view or share the source code Bitcoin was built upon.
+Perhaps the easiest way to understand bitcoin is to think of it like the internet for money. The internet is purely digital, no single person owns or controls it, it’s borderless (meaning anyone with electricity and a device can connect to it), it runs 24/7, and people who use it can easily share data between one another. Now imagine if there was an ‘internet currency’ where everyone who used the internet could help to secure it, issue it and pay each other directly with it without having to involve a bank. That’s what bitcoin essentially is.""")
 
-    print(f'Первоначальный текст:   {sentence.text}')
-    print(f'Лемма теста:            {sentence.lemmatized}')
+    for sentence in text.sentences:
+        phrase = "decentralized cryptocurrency"
 
-    print(f'Первоначальный запрос:  {phrase}')
+        logger.info(f'Первоначальный текст:   {sentence.text}')
 
-    print(f"Супер точное вхождение: {sentence.match_exact(phrase).count}")
-    print(f"Точное вхождение:       {sentence.match_exactlemmed(phrase).count}")
-    print(f"Разбитое вхождение:     {sentence.match_participant(phrase).count}")
-    print(f"Частичное вхождение:    {sentence.match_imprecise(phrase).count}")
+        logger.info(f"Супер точное вхождение: {sentence.match_exact(phrase).count}")
+        logger.info(f"Точное вхождение:       {sentence.match_exactlemmed(phrase).count}")
+        logger.info(f"Разбитое вхождение:     {sentence.match_participant(phrase).count}")
+        logger.info(f"Частичное вхождение:    {sentence.match_imprecise(phrase).count}")
+        logger.info('-' * 50)
 
 
 if __name__ == '__main__':
